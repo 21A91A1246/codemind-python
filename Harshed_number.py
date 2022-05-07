e7db5238c27f29876376defcd6a942fc5185584a
21A91A1246 <97424408+21A91A1246@users.noreply.github.com>
2022-05-07 22:34:28 +0530

    n=int(input()) s=0 t=n while(n):     r=n%10     s=s+r     n=n//10 if(t%s==0):     print(True) else:     print(False)

--- Harshed_number.py
+++ Harshed_number.py
@@ -1,11 +1,11 @@
 n=int(input())
-temp=n
-sum=0
-while n>0:
- r=n%10
- sum=sum+r
- n=n//10
-if(temp%sum==0):
+s=0
+t=n
+while(n):
+    r=n%10
+    s=s+r
+    n=n//10
+if(t%s==0):
     print(True)
 else:
     print(False)
