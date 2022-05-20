8845bc3ef400c0a5ce17645dd369e9e949372ec9
21A91A1246 <97424408+21A91A1246@users.noreply.github.com>
2022-05-20 19:23:12 +0530

    def isprime(k):     if k==1:         return False     for i in range(2,int(k**0.5)+1):         if k%i==0:             return False     else:         return True a=int(input()) b=int(input()) c=0 for i in range(a,b+1):     if(isprime(i)):         print(i)

---
+++ Generate_Prime_Numbers_In_A_Interval_.py
@@ -0,0 +1,14 @@
+def isprime(k):
+    if k==1:
+        return False
+    for i in range(2,int(k**0.5)+1):
+        if k%i==0:
+            return False
+    else:
+        return True
+a=int(input())
+b=int(input())
+c=0
+for i in range(a,b+1):
+    if(isprime(i)):
+        print(i)
