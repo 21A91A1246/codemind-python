6c8d7067b20b664bc2ac9d816a124e4fb20db594
21A91A1246 <97424408+21A91A1246@users.noreply.github.com>
2022-06-28 18:40:27 +0530

    n=int(input()) for i in range(1,n//2):     if i*i==n:         print(True)         break else:     print(False)

--- perfect_square_root_or_not.py
+++ perfect_square_root_or_not.py
@@ -1,10 +1,7 @@
-import math
-
-# Taking the input from user
-n= int(input())
-
-root = math.sqrt(n)
-if int(root + 0.5) ** 2 == n:
-    print("True")
+n=int(input())
+for i in range(1,n//2):
+    if i*i==n:
+        print(True)
+        break
 else:
-    print("False")
+    print(False)
