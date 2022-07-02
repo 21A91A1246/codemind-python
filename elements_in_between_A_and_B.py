b9ee3aee37165ff80523f0b2e033ca879d63c883
21A91A1246 <97424408+21A91A1246@users.noreply.github.com>
2022-07-02 22:40:14 +0530

    n=int(input()) a=list(map(int,input().split())) x,y=map(int,input().split()) k=0 b=[] for i in range(n):     if a[i]>=x and a[i]<=y:         k=1         b.append(a[i]) if k==1:     print(*b) else:     print("-1")

---
+++ elements_in_between_A_and_B.py
@@ -0,0 +1,14 @@
+n=int(input())
+a=list(map(int,input().split()))
+x,y=map(int,input().split())
+k=0
+b=[]
+for i in range(n):
+    if a[i]>=x and a[i]<=y:
+        k=1
+        b.append(a[i])
+if k==1:
+    print(*b)
+else:
+    print("-1")
+        
