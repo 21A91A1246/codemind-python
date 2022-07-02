3f476bdd83cb1df746b660e48432c7a89054dc02
21A91A1246 <97424408+21A91A1246@users.noreply.github.com>
2022-07-02 22:32:23 +0530

    n=int(input()) a=list(map(int,input().split())) x,y=map(int,input().split()) max=0 k=0 for i in range(n):     if a[i]>=x and a[i]<=y:         k=1         if a[i]>max:             max=a[i] if k==1:     print(max) else:     print('-1')

--- maximum_of_numbers_between_A_and_B.py
+++ maximum_of_numbers_between_A_and_B.py
@@ -1,14 +1,17 @@
 n=int(input())
 a=list(map(int,input().split()))
 x,y=map(int,input().split())
-min=0
+max=0
 k=0
 for i in range(n):
     if a[i]>=x and a[i]<=y:
         k=1
-        if a[i]>min:
-            min=a[i]
-if k==1:            
- print(min)
+        if a[i]>max:
+            max=a[i]
+if k==1:
+    print(max)
 else:
     print('-1')
+        
+        
+        
