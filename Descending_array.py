ee46dd0ee9d43d7448789b46191a490e0d76ed1c
21A91A1246 <97424408+21A91A1246@users.noreply.github.com>
2022-07-04 21:18:42 +0530

    n=int(input()) a=list(map(int,input().split())) b=[] for i in range(n):     b.append(a[i])
    c=b[::-1]
    a.sort()
    #print(c)
    #print(a)
    if c==a:
        print('yes')
    else:
        print('no')

--- Descending_array.py
+++ Descending_array.py
@@ -3,10 +3,16 @@ a=list(map(int,input().split()))
 b=[]
 for i in range(n):
     b.append(a[i])
+
 c=b[::-1]
 a.sort()
-
+#print(c)
+#print(a)
 if c==a:
     print('yes')
 else:
     print('no')
+
+
+
+
