42c9cbb0269c238ff9bcb8b418a5b9747d7ae5d1
21A91A1246 <97424408+21A91A1246@users.noreply.github.com>
2022-07-07 18:47:10 +0530

    a,b=map(int,input().split()) #print(a,b) c=list(map(int,input().split())) #print(c) d=0 for i in c:     if i<0:         i=i*-1     i=str(i)     if len(i)==b:         d=d+1 print(d)

---
+++ K_digit_length_numbers_in_a_array.py
@@ -0,0 +1,13 @@
+a,b=map(int,input().split())
+#print(a,b)
+c=list(map(int,input().split()))
+#print(c)
+d=0
+for i in c:
+    if i<0:
+        i=i*-1
+    i=str(i)
+    if len(i)==b:
+        d=d+1
+print(d)
+        
