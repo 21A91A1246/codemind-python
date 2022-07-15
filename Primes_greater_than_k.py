07651390b17ad691ae85472631dcf5d0a209ff32
21A91A1246 <97424408+21A91A1246@users.noreply.github.com>
2022-07-15 10:26:23 +0530

    def fun(n):     for i in range(2,n):         if n%i==0:             return 0     else:         return 1 n=int(input()) a=list(map(int,input().split())) m=int(input()) c=0 for i in range(n):     if a[i]==1:         continue     if(fun(a[i])) and a[i]>=m:         c+=1 print(c)

--- Primes_greater_than_k.py
+++ Primes_greater_than_k.py
@@ -1,20 +1,21 @@
-def prime(n):
+def fun(n):
     for i in range(2,n):
-        if(n%i==0):
+        if n%i==0:
             return 0
     else:
         return 1
 n=int(input())
 a=list(map(int,input().split()))
-x=int(input())
+m=int(input())
 c=0
-b=[]
 for i in range(n):
-    if a[i]>=x:
-        b.append(a[i])
-for i in range(len(b)):
-    if b[i]==1:
+    if a[i]==1:
         continue
-    if prime(b[i]):
+    if(fun(a[i])) and a[i]>=m:
         c+=1
 print(c)
+        
+
+        
+    
+        
