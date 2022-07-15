0013aee14a5251e666558da75bfb71d101f49208
21A91A1246 <97424408+21A91A1246@users.noreply.github.com>
2022-07-15 21:55:52 +0530

    n=input() a=list(map(int,input().split())) m=int(input()) m=a.index(m) s=0 for i in range(m+1):     s=s+a[i] print(s)

--- sum_of_array__elements_upto__Kth__number_in_an_array.py
+++ sum_of_array__elements_upto__Kth__number_in_an_array.py
@@ -1,5 +1,8 @@
-n=int(input())
+n=input()
 a=list(map(int,input().split()))
-x=int(input())
-b=a[:x:1]
-print(sum(b))
+m=int(input())
+m=a.index(m)
+s=0
+for i in range(m+1):
+    s=s+a[i]
+print(s)    
