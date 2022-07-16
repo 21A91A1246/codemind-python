365bf6cd6107593ba9d9213554e7921d56671b0c
21A91A1246 <97424408+21A91A1246@users.noreply.github.com>
2022-07-16 11:32:48 +0530

    n=int(input()) a=list(map(int,input().split())) c=0 for i in range(n-2):     if a[i]%2==0 and a[i+2]%2==0:         if a[i+1]%2==0:             c+=1 print(c)

--- count_even_numbers_between_even_numbers.py
+++ count_even_numbers_between_even_numbers.py
@@ -1,9 +1,9 @@
 n=int(input())
 a=list(map(int,input().split()))
 c=0
-
-for i in range(n):
-    if a[i]%2==0:
-        c+=1
-        n=c//2
-print(n)
+for i in range(n-2):
+    if a[i]%2==0 and a[i+2]%2==0:
+        if a[i+1]%2==0:
+            c+=1
+print(c)
+        
