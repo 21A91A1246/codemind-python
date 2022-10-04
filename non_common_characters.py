c5d3c5b81b21af1a1d0bf55f2d2a256a131483cb
21A91A1246 <97424408+21A91A1246@users.noreply.github.com>
2022-10-04 22:11:34 +0530

    s=input() s=s.lower() s1=input() s1=s1.lower() a=[] c=0 for i in range(len(s)):     if s[i]==' ':         continue     if s[i] not in s1 and s[i] not in a:         a.append(s[i]) for i in range(len(s1)):     if s1[i]==' ':         continue     if s1[i] not in s and s1[i] not in a:         a.append(s1[i]) a=sorted(a) for i in a:     print(i,end='')

--- non_common_characters.py
+++ non_common_characters.py
@@ -3,7 +3,7 @@ s=s.lower()
 s1=input()
 s1=s1.lower()
 a=[]
-b=[]
+c=0
 for i in range(len(s)):
     if s[i]==' ':
         continue
@@ -14,8 +14,6 @@ for i in range(len(s1)):
         continue
     if s1[i] not in s and s1[i] not in a:
         a.append(s1[i])        
-for i in range(len(a)):
-    b.append(ord(a[i]))
-b=sorted(b)
-for i in range(len(b)):
-    print(chr(b[i]),end='')
+a=sorted(a)
+for i in a:
+    print(i,end='')
