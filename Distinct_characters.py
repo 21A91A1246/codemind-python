4fceccee30008467929a121e6b8b3625b997371c
21A91A1246 <97424408+21A91A1246@users.noreply.github.com>
2022-10-04 15:19:00 +0530

    s=input() s=s.lower() a=[] b=[] for i in range(len(s)):     if s[i]==' ':         continue     if s.count(s[i])==1:         a.append(s[i]) b=sorted(a) for i in b:     print(i,end='')

---
+++ Distinct_characters.py
@@ -0,0 +1,12 @@
+s=input()
+s=s.lower()
+a=[]
+b=[]
+for i in range(len(s)):
+    if s[i]==' ':
+        continue
+    if s.count(s[i])==1:
+        a.append(s[i])
+b=sorted(a)
+for i in b:
+    print(i,end='')
