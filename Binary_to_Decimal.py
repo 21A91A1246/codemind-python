9a7e5a9ffd6a2439c6842fd892cb54f35291f070
Bharat2303 <97424408+21A91A1246@users.noreply.github.com>
2022-12-05 18:32:42 +0530

    def decimal(n):     dec,i=0,0     s=len(n)     while i<len(n):         dec+=int(n[s-1-i])*pow(2,i)         i+=1     return(dec) for i in range(int(input())):     a=input()     print(decimal(a))

--- Binary_to_Decimal.py
+++ Binary_to_Decimal.py
@@ -1,5 +1,12 @@
-#=int(input())
-
+def decimal(n):
+    dec,i=0,0
+    s=len(n)
+    while i<len(n):
+        dec+=int(n[s-1-i])*pow(2,i)
+        i+=1
+    return(dec)
 for i in range(int(input())):
-    s=input()
-    print(int(s,2))
+    a=input()
+    print(decimal(a))
+    
+    
