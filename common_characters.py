f63db80a99796e44bbbf132876d7abb129412764
Bharat2303 <97424408+21A91A1246@users.noreply.github.com>
2022-12-13 12:03:03 +0530

    s1=input() s1=s1.lower() s2=input() s2=s2.lower() c='' for i in s1:     if i in s2 and i!=' ':         c+=i c=set(c) c=sorted(c) s='' f=0 for i in c:     s+=i     f=1 if f==0:     print(-1) else:     print(s)

--- common_characters.py
+++ common_characters.py
@@ -1,22 +1,20 @@
-a=input()
-a=a.lower()
-b=input()
-b=b.lower()
-k=0
-c=[]
-for i in a:
-    if i in b:
-        c.append(i)
+s1=input()
+s1=s1.lower()
+s2=input()
+s2=s2.lower()
+c=''
+for i in s1:
+    if i in s2 and i!=' ':
+        c+=i
 c=set(c)
 c=sorted(c)
-z=''
+s=''
+f=0
 for i in c:
-    if i==' ':
-        continue
-    else:
-        z+=(i)
-        k=1
-if k==0:
+    s+=i
+    f=1
+if f==0:
     print(-1)
 else:
-    print(z)
+    print(s)
+    
